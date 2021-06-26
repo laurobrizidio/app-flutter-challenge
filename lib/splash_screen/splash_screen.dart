bb2aa4a715e4ef4b08a5063f5704166450a80081
Lauro Brizidio <lauro.brizidio@hotmail.com>
2021-06-25 22:06:36 -0400

    feat: init HomeScreen

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,11 +1,52 @@
+import 'dart:async';
+
+import 'package:app_flutter_challenge/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.white,
-    ));
+      home: SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SplashInitState();
+  }
+}
+
+class SplashInitState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Loading"),
+              CircularProgressIndicator(
+                value: 100,
+                strokeWidth: 2,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
